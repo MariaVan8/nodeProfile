06a85f6b50fbfc070235c1eb34c10135a07634e3
Sydnee Snowball <s.snowball@hotmail.com>
2023-11-22 16:33:19 -0800

    working apiRouter, changed port number, updated json file, updated contact page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,21 +10,27 @@ indexRouter.get("/about", (req, res) => res.render("about",{
     title:"About us page!"
 }));
 
-indexRouter.get("/contact", (req, res) => {
+indexRouter.get("/contact", (req, res) => 
+    res.render("contact", {status: null, title: "Contact Us"}));
+
+indexRouter.post("/contact", (req, res) => 
+    res.render("contact", {status: 'received', formData: req.body, title: "Contact Us"}));
+
+// indexRouter.get("/contact", (req, res) => {
     
-    res.render("contact",{
-        title:"Contact Page!",
-        messageSubmitted: null
-    });
-});
+//     res.render("contact",{
+//         title:"Contact Page!",
+//         messageSubmitted: null
+//     });
+// });
 
 
-//POST
-indexRouter.post("/contact", (req, res) =>{
+// //POST
+// indexRouter.post("/contact", (req, res) =>{
 
-    res.render("contact",{
-        messageSubmitted: true
-    });
-});
+//     res.render("contact",{
+//         messageSubmitted: true
+//     });
+// });
 
 module.exports = indexRouter;
